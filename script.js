d1f0e5a3251330fcb91d2630301b16f0c9d926fb
Dee2500 <darnellvincent5@icloud.com>
2024-10-31 14:15:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById("password").addEventListener("input", checkStrength);
+document.getElementById("toggleVisibility").addEventListener("click", togglePasswordVisibility);
+document.getElementById("toggleDarkMode").addEventListener("click", toggleDarkMode);
+
 function checkStrength() {
     const password = document.getElementById("password").value;
     const strengthMessage = document.getElementById("strengthMessage");
@@ -50,6 +54,18 @@ function checkStrength() {
     strengthBar.style.width = width;
 }
 
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("password");
+    const toggleButton = document.getElementById("toggleVisibility");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleButton.innerText = "Hide";
+    } else {
+        passwordInput.type = "password";
+        toggleButton.innerText = "Show";
+    }
+}
+
 function generatePassword() {
     const length = parseInt(document.getElementById("length").value);
     if (isNaN(length) || length < 12) {
@@ -57,10 +73,41 @@ function generatePassword() {
         return;
     }
 
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+    const includeUpper = document.getElementById("includeUpper").checked;
+    const includeLower = document.getElementById("includeLower").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const includeSpecial = document.getElementById("includeSpecial").checked;
+
+    const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+    
+    let characters = "";
+    if (includeUpper) characters += upperChars;
+    if (includeLower) characters += lowerChars;
+    if (includeNumbers) characters += numberChars;
+    if (includeSpecial) characters += specialChars;
+
+    if (characters.length === 0) {
+        alert("Please select at least one character type.");
+        return;
+    }
+
     let password = "";
     for (let i = 0; i < length; i++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     document.getElementById("generatedPassword").innerText = password;
+}
+
+function copyPassword() {
+    const password = document.getElementById("generatedPassword").innerText;
+    navigator.clipboard.writeText(password)
+        .then(() => alert("Password copied to clipboard!"))
+        .catch(err => console.error("Error copying password: ", err));
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
 }
